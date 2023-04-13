af14f46534c5c4200927ac2061956116bf36f6f6
Alexa Lewis <alexalewis2013@afscholars.org>
2023-04-12 23:17:57 -0400

    repos added

--- src/main/java/com/example/todolist/Repository/AccountRepository.java
+++ src/main/java/com/example/todolist/Repository/AccountRepository.java
@@ -1,2 +1,7 @@
-package com.example.todolist.Repository;public interface AccountRepository {
+package com.example.todolist.Repository;
+
+import com.example.todolist.Model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
 }
--- src/main/java/com/example/todolist/Repository/TaskRepository.java
+++ src/main/java/com/example/todolist/Repository/TaskRepository.java
@@ -1,2 +1,7 @@
-package com.example.todolist.Repository;public interface TaskRepository {
+package com.example.todolist.Repository;
+
+import com.example.todolist.Model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task,Long> {
 }
